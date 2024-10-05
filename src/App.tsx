e84b406ce98fd95f4dccc125b3b08dac0a1ca0a9
Gary Varner <garysarahvarner@gmail.com>
2024-10-04 20:28:14 -0400

    cleaning up the main page a bit

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,14 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { removeBackground } from "@imgly/background-removal";
 
 function App() {
-  const [count, setCount] = useState(0);
   const [blob, setBlob] = useState("");
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
         <input
           type="file"
           name="file"
@@ -43,11 +26,8 @@ function App() {
             });
           }}
         />
-        {blob && <img src={blob} width={300} height={300} />}
+        {blob && <img src={blob} width={600} height={400} />}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
