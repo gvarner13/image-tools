2336a0d2d7712b49626777a23a30759a17c9095b
Gary Varner <garysarahvarner@gmail.com>
2024-10-06 22:03:53 -0400

    adding exif info package

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import { removeBackground } from "@imgly/background-removal";
 // import gsap from "https://cdn.skypack.dev/gsap@3.12.0";
+import ExifReader from "exifreader";
 
 function App() {
   const [blob, setBlob] = useState("");
@@ -21,7 +22,7 @@ function App() {
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1>👻 Image Tools</h1>
       <div style={{ display: "flex" }} onPointerMove={handlePointerMove}>
         <div>
           <article>
@@ -62,7 +63,8 @@ function App() {
             </div>
           </article>
         </div>
-        <div>
+        <div style={{ width: "600px" }}>
+          {loading && <div>Loading ...</div>}
           {blob && (
             <article>
               <img src={blob} alt="" />
@@ -75,25 +77,6 @@ function App() {
               />
               <div></div>
             </div> */}
-              <div className="content">
-                <p>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                    className="w-6 h-6"
-                  >
-                    <path d="M15.75 8.25a.75.75 0 0 1 .75.75c0 1.12-.492 2.126-1.27 2.812a.75.75 0 1 1-.992-1.124A2.243 2.243 0 0 0 15 9a.75.75 0 0 1 .75-.75Z" />
-                    <path
-                      fill-rule="evenodd"
-                      d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM4.575 15.6a8.25 8.25 0 0 0 9.348 4.425 1.966 1.966 0 0 0-1.84-1.275.983.983 0 0 1-.97-.822l-.073-.437c-.094-.565.25-1.11.8-1.267l.99-.282c.427-.123.783-.418.982-.816l.036-.073a1.453 1.453 0 0 1 2.328-.377L16.5 15h.628a2.25 2.25 0 0 1 1.983 1.186 8.25 8.25 0 0 0-6.345-12.4c.044.262.18.503.389.676l1.068.89c.442.369.535 1.01.216 1.49l-.51.766a2.25 2.25 0 0 1-1.161.886l-.143.048a1.107 1.107 0 0 0-.57 1.664c.369.555.169 1.307-.427 1.605L9 13.125l.423 1.059a.956.956 0 0 1-1.652.928l-.679-.906a1.125 1.125 0 0 0-1.906.172L4.575 15.6Z"
-                      clip-rule="evenodd"
-                    />
-                  </svg>
-                  <span>Osaka Castle</span>
-                </p>
-                <p>Osaka, Japan</p>
-              </div>
             </article>
           )}
         </div>
@@ -103,10 +86,12 @@ function App() {
           type="file"
           name="file"
           id="file"
-          onChange={(e) => {
+          onChange={async (e) => {
             setLoading(true);
             const [file]: any = e.target.files;
             setBlob(URL.createObjectURL(file));
+            const tags = await ExifReader.load(file);
+            console.log({ tags });
             removeBackground(file).then((blob: Blob) => {
               setNoBgBlob(URL.createObjectURL(blob));
               setTimeout(() => {
@@ -116,13 +101,11 @@ function App() {
             });
           }}
         />
-        <div>
-          <input
-            name="myInput"
-            value={bgText}
-            onChange={(e) => setBgText(e.target.value)}
-          />
-        </div>
+        <input
+          name="myInput"
+          value={bgText}
+          onChange={(e) => setBgText(e.target.value)}
+        />
         {/* {blob && <img src={blob} width={600} height={400} />} */}
       </div>
     </>
