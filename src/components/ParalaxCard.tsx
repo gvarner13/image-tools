f2daa60a2458322149a1e3a7cd851195e6aac832
Gary Varner <garysarahvarner@gmail.com>
2024-10-14 22:17:30 -0400

    wip

--- src/components/ParalaxCard.tsx
+++ src/components/ParalaxCard.tsx
@@ -1,11 +1,9 @@
-// Declaring type of props - see "Typing Component Props" for more examples
 type AppProps = {
   title: string;
   bgImage: string;
   fgImage: string;
-}; /* use `interface` if exporting so that consumers can extend */
+};
 
-// Easiest way to declare a Function Component; return type is inferred.
 const ParalaxCard = ({ title, bgImage, fgImage }: AppProps) => {
   return (
     <article>
